15515f61ed7d345970a22b58a8db83e712335d80
Natalie Kadonaga <nat.kadonaga@gmail.com>
2018-02-11 18:16:46 -0800

    Add minimum password requirements

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
                     uniqueness: true,
                     uniqueness: { case_sensitive: false }
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
