9b814a04f1a0562ff46ff486a4815d3bc7bf4714
Natalie Kadonaga <nat.kadonaga@gmail.com>
2018-02-17 02:48:05 -0800

    Add user microposts

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -26,6 +26,5 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", users_path
-    assert_select "a[href=?]", signup_path
   end
 end
