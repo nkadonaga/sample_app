9cb35e2a7d7d90c0da5c34078bfaf88799ea8810
Natalie Kadonaga <nat.kadonaga@gmail.com>
2018-02-17 03:02:18 -0800

    Fix typo

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -4,7 +4,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
 
-  storage: file
+  storage :file
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
